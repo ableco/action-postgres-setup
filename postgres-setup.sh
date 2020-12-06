2b0aea51435972aad70e4cfd8b668c9e7af5ab08
Hector Paz <hector@able.co>
2020-12-05 21:39:51 -0500

    Iniatialize pgbench

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -3,7 +3,7 @@ POSTGRES_VERSION=$1
 MAX_CONNECTIONS=$2
 if [ $POSTGRES_VERSION -ne 13 ]; then
   sudo apt-get remove postgresql postgresql-13
-  sudo apt-get install postgresql-$POSTGRES_VERSION
+  sudo apt-get install postgresql-$POSTGRES_VERSION postgresql-contrib-$POSTGRES_VERSION
   sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
 fi
 sudo bash -c 'echo fsync=off >> /etc/postgresql/'$POSTGRES_VERSION'/main/postgresql.conf'
